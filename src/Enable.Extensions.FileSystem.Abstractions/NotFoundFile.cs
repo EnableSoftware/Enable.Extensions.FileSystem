ddd2b85f53326ba284c9c0ca40ad0881e45aab0c
Chris Wrench <c.g.wrench@gmail.com>
2017-10-25 10:19:43 +0100

    Update solution to VS2017

--- src/Enable.Extensions.FileSystem.Abstractions/NotFoundFile.cs
+++ src/Enable.Extensions.FileSystem.Abstractions/NotFoundFile.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Enable.Extensions.FileSystem
 {
--- src/Enable.Extensions.FileSystem.Physical/Properties/AssemblyInfo.cs
+++ src/Enable.Extensions.FileSystem.Physical/Properties/AssemblyInfo.cs
@@ -1,13 +1,3 @@
-﻿using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Enable.Extensions.FileSystem.Physical")]
-[assembly: AssemblyDescription("Physical file system abstraction implementation.")]
-[assembly: AssemblyCompany("Enable · enable.com")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: InternalsVisibleTo("Enable.Extensions.FileSystem.Physical.Tests")]
+[assembly:InternalsVisibleTo("Enable.Extensions.FileSystem.Physical.Tests")]
